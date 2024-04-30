42678d022c4de27cf59d940b5c9147aca6865ed3
nomico <nomicomateou@gmail.com>
2024-04-30 11:28:53 -0300

    initial commit

---
+++ ej10.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+#|
+    Definir una función que dada la base y la altura de un triángulo devuelva el valor del área.
+ 		> (superficieTriangulo 2 4)  =>  4
+|#
+
+(define (superficieTriangulo base altura) (* 1/2 base altura))
