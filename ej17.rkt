607922bcf1993c890292016c2a28b662445b5af1
nomico <nomicomateou@gmail.com>
2024-04-30 14:00:23 -0300

    add comments

--- ej17.rkt
+++ ej17.rkt
@@ -5,19 +5,25 @@
         > (hms 7200)  =>  (2 0 0) 
 |#
 
+; Función para calcular las horas
 (define(segundosAHoras segundos)
-    (quotient segundos 3600)
+    (quotient segundos 3600)    ; Para pasar de segundos a minutos hay que dividir a los segundos por 60, y para pasar de minutos a horas también, 
+                                ; entonces para pasar de segundos a horas directamente, divido los segundos por 60 * 60 = 3600
 )
 
+; Función para calcular los minutos restantes que no llegan a formar una hora
+; Ejemplo: 64 minutos = 1 hora 4 minutos
 (define (segundosAMinutos segundos)
     (remainder
-        (quotient segundos 60)
-        60
-    )
+        (quotient segundos 60)  ; Convierto los segundos en minutos
+        60                      ; Divido por 60 para convertirlos en horas, y me quedo con el resto
+    ) 
 )
 
+; Función para calcular los segundos restantes que no llegan a formar un minuto
+; Ejemplo: 84 segundos = 1 minuto 24 segundos
 (define (segundosRestantes segundos)
-    (remainder segundos 60)
+    (remainder segundos 60)     ; Divido por 60 para convertirlos en minutos y me quedo con el resto
 )
 
 ; Función principal
