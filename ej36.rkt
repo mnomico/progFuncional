32856b0d8ea2536409820f255a3086ed46ecf943
nomico <nomicomateou@gmail.com>
2024-05-01 14:36:28 -0300

    some changes

--- ej36.rkt
+++ ej36.rkt
@@ -7,9 +7,8 @@ Inicialmente el tanque tiene L litros.
 Desarrollar una función que acepte los valores de T, C, S y L, y que calcule cuántos minutos tarda en llenarse o vaciarse el tanque.
 |#
 
-#|
-    Hay que hacer una función que verifique que por cada minuto que pase si el tanque se llenó o se vació
-|#
+
+; En resumen, hay que hacer una función que verifique que por cada minuto que pase si el tanque se llenó o se vació
 
 ; Función que utilizo para retornar el resultado
 (define (concatenarString cadena1 numero cadena2)
@@ -18,13 +17,17 @@ Desarrollar una función que acepte los valores de T, C, S y L, y que calcule cu
 
 ; Función recursiva que verifica si el tanque se llenó o vació, retornando la cantidad de minutos que transcurrieron en el proceso
 (define (verificarEstado capacidadTanque diferenciaLitros litrosActuales minutosTranscurridos)
-    (define litrosSiguientes (+ litrosActuales diferenciaLitros))   ; Defino los litros que va a tener el tanque en el próximo minuto (o llamada recursiva)
-    (define minutoSiguiente (+ minutosTranscurridos 1))             ; Defino el minuto siguiente para pasarlo como parámetro a la llamada recursiva
+
+    (define litrosSiguientes (+ litrosActuales diferenciaLitros))   ; Defino los litros que va a tener el tanque en la llamada recursiva siguiente
+    (define minutoSiguiente (+ minutosTranscurridos 1))             ; Defino el minuto siguiente para la llamada recursiva siguiente
+
     (cond
         ; Si los litros que tiene el tanque son iguales o mayores a su capacidad, entonces se llenó
         [(>= litrosActuales capacidadTanque) (concatenarString "El tanque se llenó en " minutosTranscurridos " minutos")]
+
         ; Si los litros que tiene el tanque son menores o iguales a 0 entonces el tanque está vacío
         [(<= litrosActuales 0) (concatenarString "El tanque se vació en " minutosTranscurridos " minutos")]
+
         ; Si no ocurre ninguna de las dos cosas, hago la llamada recursiva pasando como parámetros los datos definidos anteriormente
         [else (verificarEstado capacidadTanque diferenciaLitros litrosSiguientes minutoSiguiente)]
     )
@@ -32,9 +35,11 @@ Desarrollar una función que acepte los valores de T, C, S y L, y que calcule cu
 
 ; Función principal.
 (define (calcularTiempo capacidadTanque litrosEntrantes litrosSalientes litrosIniciales)
+
     ; Calculo la diferencia que hay entre los litros entrantes y los salientes. Si el resultado es positivo, hay más litros que entran de los que salen,
     ; entonces el tanque se va a llenar. Si el resultado es negativo, salen más litros de los que entran, entoncesel tanque se va a vaciar.
     (define diferenciaLitros (- litrosEntrantes litrosSalientes))       
+
     ; Hago la llamada recursiva pasando la capacidad del tanque, la diferencia de litros, los litros iniciales y los minutos transcurridos, que serían inicialmente 0.    
-    (verificarEstado capacidadTanque diferenciaLitros litrosIniciales 0)
+    (verificarEstado capacidadTanque diferenciaLitros litrosIniciales 0) 
 )
