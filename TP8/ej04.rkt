ee193b897515762e7d6c8be49bc9136acfc3305f
nomico <nomicomateou@gmail.com>
2024-05-31 14:08:23 -0300

    refactor cantidadNaturales

--- TP8/ej04.rkt
+++ TP8/ej04.rkt
@@ -6,14 +6,10 @@ Escribir una función que acepte una lista de números enteros y devuelva la can
 > (cantidadNaturales ‘(-1 2 3 -4 5 0 2))  => 4
 |#
 
-(define (calcularNaturales lista cantidad)
-    (cond
-        [(empty? lista) cantidad]
-        [(and (number? (first lista)) (> (first lista) 0)) (calcularNaturales (rest lista) (+ cantidad 1))]
-        [else (calcularNaturales (rest lista) cantidad)]
-    )
-)
-
 (define (cantidadNaturales lista)
-    (calcularNaturales lista 0)
+    (cond
+        [(empty? lista) 0]
+        [(natural? (first lista)) (+ (cantidadNaturales (rest lista)) 1)]
+        [else (cantidadNaturales (rest lista))]
+    )
 )
